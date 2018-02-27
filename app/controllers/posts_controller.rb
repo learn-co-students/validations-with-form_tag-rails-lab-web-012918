9bb6db1e0956b3b38d42461aae5ec44157ae7bb7
brookescarlett <brookeyalof2017@u.northwestern.edu>
2018-02-27 13:26:11 -0500

    DONE

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post!, only: [:show, :edit, :update]
+
   def show
     @post = Post.find(params[:id])
   end
@@ -8,16 +10,24 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.find(params[:id])
-
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
-  end
+   @post.title = params[:title]
+   @post.content = params[:content]
+   @post.category = params[:category]
+   if @post.valid?
+     @post.update(post_params)
+     redirect_to post_path(@post)
+   else
+     render :edit
+   end
+ end
 
   private
 
   def post_params
     params.permit(:title, :category, :content)
+  end
+
+  def set_post!
+    @post = Post.find(params[:id])
   end
 end
