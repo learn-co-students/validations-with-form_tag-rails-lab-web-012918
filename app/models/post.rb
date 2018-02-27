ea5776389a187ae8b08bf48ed8f3fef0186aae4b
porzingod <jasonlim.ny@gmail.com>
2018-02-27 13:27:18 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 20 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} must be Fiction or Non-Fiction" }
 end
